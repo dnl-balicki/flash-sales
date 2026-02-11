3c8234bcb8821cf1d38b04a96229746458af61c8
dnl-balicki <danielbalicki023@gmail.com>
2026-02-11 16:51:10 +0100

    chore(infra): add testcontainers configuration

--- src/test/java/com/balicki/flashsales/TestcontainersConfiguration.java
+++ src/test/java/com/balicki/flashsales/TestcontainersConfiguration.java
@@ -13,13 +13,14 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+        return new KafkaContainer(
+                DockerImageName.parse("apache/kafka:3.7.0")
+                        .asCompatibleSubstituteFor("confluentinc/cp-kafka"));
     }
 
     @Bean
     @ServiceConnection
     PostgreSQLContainer postgresContainer() {
-        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:16"));
     }
-
 }
